4e97bb7c2fa797806edb1200e5d99b9bf4e342e1
brumab <brunomolinasouza1986@gmail.com>
2025-09-07 18:04:02 -0300

    Resolve conflito e mescla tudo

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
 
-    path('estudantes/', views.estudantesView)
+    path('estudantes/', views.estudantesView),
+    path('estudantes/<int:pk>/', views.estudantesDetailView),
+
+    path('employees/', views.Employees.as_view()),
+    path('employees/<int:pk>/', views.EmployeesDetail.as_view()),
+
 
 ]
